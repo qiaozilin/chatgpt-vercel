89bd073a9db5c8a157a4508666a2abf8f72ae795
qiaozilin <59814739+qiaozilin@users.noreply.github.com>
2023-07-21 15:58:07 +0800

    Update index.tsx

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -4,6 +4,8 @@ import type { JSXElement } from "solid-js"
 export default function ({ children }: { children: JSXElement }) {
   return (
     <div id="root" class="sm:pt-8em py-2em before">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4690058608545443"
+         crossorigin="anonymous"></script>
       <Header />
       {children}
     </div>
